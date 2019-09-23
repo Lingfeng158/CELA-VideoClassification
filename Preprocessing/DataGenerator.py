7c9b42df669e12e446d96fef30a4e2a125e0b577
lingfengli <lingfeng.li@vanderbilt.edu>
2019-09-23 17:02:17 -0500

    DataGen

---
+++ Preprocessing/DataGenerator.py
@@ -0,0 +1,44 @@
+import numpy as np
+import keras
+
+class imgDataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, dataPath, batch_size=1, shuffle=True):
+        'Initialization'
+        self.dataPath = dataPath
+        self.shuffle = shuffle
+        self.on_epoch_end()
+        
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.labels) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+
+        # Generate data
+        X, y = self.__data_generation(index)
+
+        return X, y
+
+    def on_epoch_end(self):
+        """
+        Updates indexes after each epoch
+        reload &update Datapath
+        """
+        #dataCohort: #groupsize,#videolength, pathToData
+        self.dataCohort=prepdata(self.dataPath)
+        self.labels,self.data=zip(*self.dataCohort)
+        self.indexes = np.arange(len(self.dataCohort))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, ID):
+        'Generates data containing batch_size samples'
+        """
+        Speciallized for this Deep learning task, ID should be a single number
+        """
+        loadedData=imgListLoader(self.data[ID])
+        dataLabel=self.labels[ID]
+
+        return loadedData, dataLabel
